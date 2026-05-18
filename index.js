860f30ca7563a8188b15135f6af12eb4f7264151
macbookpro <macbookpro@Nguyens-MacBook-Pro.local>
2026-05-18 12:32:24 +0530

    Xay dung layout chung

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
 const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
+
+
 
 database.connect();
 
@@ -13,6 +17,9 @@ const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+//App local variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.use(express.static("public"));
 
