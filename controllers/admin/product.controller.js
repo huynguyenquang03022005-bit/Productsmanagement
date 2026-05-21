fcabad246dc148ff22d954f4ac29de06c8f27db0
macbookpro <macbookpro@Nguyens-MacBook-Pro.local>
2026-05-21 23:12:14 +0530

    Tinh nang tim kiem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -36,13 +36,24 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
 
+
+  let keyword = "";
+
+  if(req.query.keyword) {
+    keyword = req.query.keyword;
+    
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
   const products = await Product.find(find);
+
 
   // console.log(products);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
-    filterStatus: filterStatus
+    filterStatus: filterStatus,
+    keyword: keyword
   });
 }
