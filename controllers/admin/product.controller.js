025ed092f7117d72a52595d81bd1cc6c72dbdc94
macbookpro <macbookpro@Nguyens-MacBook-Pro.local>
2026-05-22 12:43:06 +0530

    toi uu phan tim kiem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,7 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
-
+const searchHelper = require("../../helpers/search");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
   
@@ -16,24 +16,19 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
 
+  const objectSearch = searchHelper(req.query);
 
-  let keyword = "";
-
-  if(req.query.keyword) {
-    keyword = req.query.keyword;
-
-    const regex = new RegExp(keyword, "i");
-    find.title = regex;
+  if(objectSearch.regex) {
+    find.title = objectSearch.regex;
   }
+
   const products = await Product.find(find);
 
-
-  // console.log(products);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword
+    keyword: objectSearch.keyword
   });
 }
